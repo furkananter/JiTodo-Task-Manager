737be8cc34a41001444695652b4dc98513f358e0
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-19 21:59:35 +0300

    First Loading

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const style = {
+  main: "w-full h-full flex justify-center items-center bg-red-600",
+}
+
+let name: string;
+name = "asd";
+
+function App() {
+  return (
+    <div className={style.main}>asdad
+    </div>
+  );
+}
+
+export default App;
