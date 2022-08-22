52fa23ac20feb2c2323953d192ea5c71f7976034
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-23 02:20:22 +0300

    Mainly Single Todo Improved

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,45 @@ import InputField from "components/InputField";
 import { Todo } from "./model";
 import TodoList from "components/TodoList";
 import { idText } from "typescript";
-// url1 =https://images.unsplash.com/photo-1604076850742-4c7221f3101b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2731&q=80 
-// url2 = 
 
+// Dark Mode:
+export const ThemeContext = React.createContext("light");
 
 const styleApp = {
   main: `w-full min-h-screen flex flex-col px-8 md:px-20 lg:px-60 bg-[url('./components/img/light.jpeg')] dark:bg-[url('./components/img/dark.jpeg')] bg-cover text-center`,
   logo: "text-3xl text-black dark:text-white mt-3.5 z-1 font-semibold text-center md:text-4xl md:my-6",
-}
+};
 
 const App: React.FC = () => {
-  
+  //------------------------------------------------------------
+  // dark mode:
+  // const [toggle, setToggle] = useState("light");
+  // const toggleTheme = () => {
+  //   setToggle((curr) => (curr === "light" ? "dark" : "light"));
+  // };
+  //------------------------------------------------------------
+
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Array<Todo>>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-    
+    //------------------------------------------------------------
     // ENG: When we add a todo, we want to add it to the todos array.
     // ENG: It should set our State.
-
     // TR: Yeni bir todo eklediğimizde bu todo'nun state'imizi güncellemesi gerekir.
+    //------------------------------------------------------------
     if (todo.length > 0) {
       setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
       setTodo("");
-   
     }
   };
-console.log(todos)
+  console.log(todos);
   return (
     <div className={styleApp.main}>
-      <span className={styleApp.logo}>
-        JiTodo
-      </span> {/* Logo */}
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} /> {/* InputField */}
+      <span className={styleApp.logo}>JiTodo</span> {/* Logo */}
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />{" "}
+      {/* InputField */}
       <TodoList todos={todos} setTodos={setTodos} /> {/* TodoList */}
     </div> /* Main */
   );
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,8 +1,11 @@
-
 import { Todo } from "../model";
 import { IoTrashBinOutline } from "react-icons/io5";
-import {MdDone,MdEdit,MdEditOff, MdDoneAll} from "react-icons/md";
+import { MdDone, MdEdit, MdEditOff, MdDoneAll } from "react-icons/md";
+import React, { useEffect, useState, useRef } from "react";
 
+
+
+//--------------------------------------------------------------------------------
 // This type is for defining the props of the component.
 // Todo is the type of the todo.
 // todos is an array of Todo's.
@@ -12,6 +15,7 @@ import {MdDone,MdEdit,MdEditOff, MdDoneAll} from "react-icons/md";
 //* TR: Todo tipi todo'yu tanımlar.
 //* TR: todos dizisi Todos'un tipini gösterir.
 //* TR: setTodos fonksiyonu todos dizisinin state'ini güncelleyen bir fonksiyonudur.
+//--------------------------------------------------------------------------------
 
 type Props = {
   todo: Todo;
@@ -19,47 +23,76 @@ type Props = {
   setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
 };
 
+//-------------------------------------------------------------------------------
 //! ENG: SingleTodo is a component that renders a form inside a list of our Todo's.
 //! ENG: It will contain the todo's text and a button to edit the todo.
 
 //* TR: SingleTodo component'ı bir liste içinde todo'ların metni ve todo'yı düzenleme butonunu içerir.
 //* TR: SingleTodo component'ının props'ında todo, todos ve setTodos'ını alır.
+//--------------------------------------------------------------------------------
 
 const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }: Props) => {
- 
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   //! ENG: This function is used to get done the todo.
-  const handleDone = (id:number) => {
+  const handleDone = (id: number) => {
     setTodos(
-      todos.map((todo) => 
-      todo.id === id ? {...todo, isDone: !todo.isDone} : todo
-     ))
-     console.log(todo.isDone)
-  }
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+    console.log(todo.isDone);
+  };
+
   //! ENG: This function is used to delete the todo.
-  const handleDelete = (id:number) => {
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  //TODO: ENG: This function is used to edit the todo.
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
     setTodos(
-      todos.filter((todo) => todo.id !== id))
-  }
-  //! ENG: This function is used to edit the todo.
-  // const handleEdit = (id:number) => {
-  //   setTodos(
-  //     todos.map((todo) => 
-  //     todo.id === id ? {...todo, isEditing: !todo.isEditing} : todo
-  //    ))
-  // }
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  // useRef Hook'u ile Edit Butonuna basıldığında direkt focus almasını sağlayacağız.
+  const editRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (edit) {
+      editRef.current?.focus();
+    }
+  }, [edit]);
 
   return (
-    <form className="flex bg-white dark:bg-black dark:text-white text-black dark:bg-opacity-20 bg-opacity-20 backdrop-blur-lg drop-shadow-lg justify-between p-4 items-center mx-2 my-4 rounded-md">
-      {todo.isDone ? (
-        <s className="whitespace-normal overflow-x-auto dark:text-white text-black opacity-60 dark:opacity-70 line-clamp-2 text-lg capitalize">{todo.todo}</s  > /* Todo lined Item */
-        ): (
-          
-        <span className="whitespace-normal overflow-x-auto dark:text-white line-clamp-1 text-lg capitalize">{todo.todo}</span> /* Todo Item */
+    <form
+      onSubmit={(e) => {
+        handleEdit(e, todo.id);
+      }}
+      className="flex bg-white dark:bg-black dark:text-white text-black dark:bg-opacity-20 bg-opacity-20 backdrop-blur-lg drop-shadow-lg justify-between p-4 items-center mx-2 my-4 rounded-md"
+    >
+      {edit ? (
+        <input
+          ref={editRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="p-2 text-green-400 bg-black bg-opacity-20 rounded-sm text-xs md:text-base lg:text-lg outline-none "
+        />
+      ) : todo.isDone ? (
+        <s className="whitespace-normal overflow-x-auto dark:text-white text-black opacity-60 dark:opacity-70 text-xs md:text-base lg:text-lg capitalize">
+          {todo.todo}
+        </s> /* Todo lined Item */
+      ) : (
+        <span className="whitespace-normal overflow-x-auto dark:text-white text-xs md:text-base lg:text-lg  capitalize">
+          {todo.todo}
+        </span> /* Todo Item */
       )}
-
-
       <div className="flex items-center justify-center">
         {/*
+          //------------------------------------------------------------------------------------
             - Eğer butonum isDone'da true döndürüyorsa bu durumda, MdEditOff iconunu çalıştır.
             - Aynı zamanda isDone true döndüğü zaman button için disabled olmasını sağlar.
             - Span için yapmak istiyordum ama bunu spana uygulayamadım. 
@@ -68,34 +101,39 @@ const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }: Props) => {
             - Butonumun çalışıp çalışmadığını görmek için console.log("Tıklandı") yazdırıyorum.
             - Butonum isDone : true iken çalışmıyor. isDone : false ise çalışıyor.
             - ekranın yenilenmesini önlemek için ise e parametresiyle e.preventDefault yapıyorum.
-            eğer 
-         
+            - bunu şu an kullanmama gerek kalmadı.
+          //--------------------------------------------------------------------------------------
         */}
         <button
-         onClick={(e) => {
-          e.preventDefault();
-          console.log("Tıklandı")
-        }}
-         className="mr-2 hover:text-black dark:hover:text-white hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md disabled:opacity-30"
-         disabled={todo.isDone ? true : false}
-         >
-          {todo.isDone ? (
-            <MdEditOff size={24} />
-            ): <MdEdit size={24} />
-          }
-        </button> {/* Edit icon */}
-        <span
-        onClick={() => handleDelete(todo.id)} 
-        className="mr-2 hover:text-black dark:hover:text-white  hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md">
-          <IoTrashBinOutline size={24} />
-        </span> {/* Delete */}
-        <span 
-        className="hover:text-black dark:hover:text-white hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md"
-        onClick={() => handleDone(todo.id)}
+          type="button"
+          onClick={() => {
+            // eğer, edit ve isDone true ise, setEdit true döndürsün. 
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+          className="mr-2 hover:text-black dark:hover:text-white hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md disabled:opacity-30"
+          disabled={todo.isDone ? true : false}
         >
-           {todo.isDone ? (<MdDoneAll size={24} />):(<MdDone size={24}/>)} 
-        </span> {/* Check */}
-      </div> {/* Edit and Delete buttons */}
+          {todo.isDone ? <MdEditOff size={24} /> : <MdEdit size={24} />}
+        </button>{" "}
+        {/* Edit icon */}
+        <span
+          onClick={() => handleDelete(todo.id)}
+          className="mr-2 hover:text-black dark:hover:text-white  hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md"
+        >
+          <IoTrashBinOutline size={24} />
+        </span>{" "}
+        {/* Delete */}
+        <span
+          className="hover:text-black dark:hover:text-white hover:bg-opacity-30 hover:bg-white opacity-80 active:scale-[0.95] p-1.5 rounded-md"
+          onClick={() => handleDone(todo.id)}
+        >
+          {todo.isDone ? <MdDoneAll size={24} /> : <MdDone size={24} />}
+        </span>{" "}
+        {/* Check */}
+      </div>{" "}
+      {/* Edit and Delete buttons */}
     </form> // Form
   );
 };
